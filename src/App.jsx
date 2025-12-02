c456be43fccf0d2b9fa8eb0235c1264aa59113b4
24001768 <24001768@hope.ac.uk>
2025-12-02 21:46:10 +0000

    ResourceManagement

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
-import AttendanceDetails from './AttendanceDetails.js';
-import LectureMissed from './LecturesMissed.js';
+import ResourceManagement from './ResourceManagement.js';
+import ResultsSection from './ResultsSection.js';
+import ProgressPlan from './ProgressPlan.js'
 
 function App() {
   return React.createElement('div', {
     className: 'min-h-screen bg-gray-100 p-5 space-y-8'
   },
-    React.createElement(AttendanceDetails, null),
-    React.createElement(LectureMissed, null)
+    React.createElement(ResourceManagement, null),
+    React.createElement(ResultsSection, null),
+    React.createElement(ProgressPlan,null)
   );
 }
 
